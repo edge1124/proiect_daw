257a609423b4b67a6e1649cd2a045074498cde52
edge1124 <stefansindelaru@gmail.com>
2026-01-05 23:30:57 +0200

    Add files via upload

---
+++ mail_config.php
@@ -0,0 +1,6 @@
+<?php
+$usernameconfirm='confirm@csindelaru.daw.ssmr.ro';
+$passwordconfirm='parolasecreta';
+$usernamecontact='contact@csindelaru.daw.ssmr.ro';
+$passwordcontact='parolasecretadinnou';
+?>
